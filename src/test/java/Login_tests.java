9fbf9a441eb537d83d305fbe104682e5d8f14c7b
Aleksei Misiura <agm@ukr.net>
2019-02-15 12:47:31 +0200

    14.02.2019

--- src/test/java/Login_tests.java
+++ src/test/java/Login_tests.java
@@ -1,43 +1,47 @@
 //import org.openqa.selenium.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Login_tests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\misiura_o\\IdeaProjects\\qaauto5022019\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver(); /* объявление переменной (имя - driver, тип - Webdriver) и задание значения - создание нового объекта хром драйвера, new - создание нового объекта */
+        driver.get("https://www.linkedin.com/");// переходим на заданную страницу
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
     @Test
     public void succesfullLoginTest(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\misiura_o\\IdeaProjects\\qaauto5022019\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver(); /* объявление переменной (имя - driver, тип - Webdriver) и задание значения - создание нового объекта хром драйвера, new - создание нового объекта */
-        driver.get("https://www.linkedin.com/");// переходим на заданную страницу
+
         //Home task 12.02.2019
         String login = "anonim3225@gmail.com" ;
         String pswd = "Qwerty12" ;
 
-        WebElement fieldLogin = driver.findElement(By.name("session_key"));//
-        WebElement fieldPassword = driver.findElement(By.name("session_password"));//
+        WebElement fieldUserEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));//
+        WebElement fieldUserPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));//
+        WebElement buttonSubmit = driver.findElement(By.xpath("//input[@id='login-submit']"));//
 
-        fieldLogin.sendKeys(login);
-        fieldPassword.sendKeys(pswd);
+        fieldUserEmailField.sendKeys(login);
+        fieldUserPasswordField.sendKeys(pswd);
 
-        WebElement buttonSubmit = driver.findElement(By.id("login-submit"));//
         buttonSubmit.sendKeys(Keys.ENTER);
-        //searchButton.click(); //альтернативный путь нажатия на кнопку
 
-        boolean ifExist;
-        try {
-            driver.findElement(By.xpath("//span[@class='t-16 t-black t-bold']"));
-            ifExist = true;
-        } catch (NoSuchElementException e) {
-            ifExist = false;
-        }
-
-        if (ifExist)
-        {
-            System.out.println("Login is succesfull");
-        }
-        else
-        {
-            System.out.println("Login is not successfull");
-        }
+        WebElement prifileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+        prifileMenuItem.isDisplayed();
+       Assert.assertTrue(prifileMenuItem.isDisplayed(),"prifileMenuItem is displayed on Home Page");
+       Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/",
+               "Home Page URL is not correct");
 }
+    @Test
+    public void negativeLoginTest (){}
 }
